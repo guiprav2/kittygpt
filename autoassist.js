dac0ae39cd230bfa28d6bcf9d08a827b2d922e2e
Camila Prá <camila@guiprav.com>
2025-05-15 20:39:18 -0300

    .

--- autoassist.js
+++ autoassist.js
@@ -1,5 +1,5 @@
 import BiMap from './bimap.js';
-import htmlsnap from './htmlsnap.js';
+import htmlsnap from 'https://esm.sh/@camilaprav/htmlsnap@0.0.7';
 import voicechat from './voicechat.js';
 
 async function fillInput(inputElement, text, delay = 50) {
